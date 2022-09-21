effe30a9661e11bebd1db5d68fd52ea977435f74
Michelle <michelle.teixeira.124@ufrn.edu.br>
2022-09-20 21:57:49 -0300

    criação do projeto

---
+++ financas_web/financas_api/src/test/java/com/imd/financas_api/FinancasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imd.financas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
