7eeb7ebd0335586defa5d3fee69630438458d445
Ana Luisa <53523588+anaestevam@users.noreply.github.com>
2022-10-30 21:04:33 -0300

    a

--- financas_api/src/main/java/com/imd/financas_api/abstracts/Ganho.java
+++ financas_api/src/main/java/com/imd/financas_api/abstracts/Ganho.java
@@ -2,15 +2,16 @@ package com.imd.financas_api.abstracts;
 
 import com.imd.financas_api.conta.model.Conta;
 import com.sun.istack.NotNull;
-import org.joda.time.DateTimeZone;
+//import org.joda.time.DateTimeZone;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import java.util.Date;
 import java.util.UUID;
 
-public abstract class Gasto {
+abstract class Gasto {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
@@ -20,7 +21,7 @@ public abstract class Gasto {
     private Double value;
     @OneToMany
     private Conta account;
-    private DateTimeZone data_vencimento;
-    private DateTimeZone data_pagamento;
+    private Date data_vencimento;
+    private Date data_pagamento;
     private boolean pago;
 }
--- financas_api/src/main/java/com/imd/financas_api/abstracts/Gasto.java
+++ financas_api/src/main/java/com/imd/financas_api/abstracts/Gasto.java
@@ -2,15 +2,16 @@ package com.imd.financas_api.abstracts;
 
 import com.imd.financas_api.conta.model.Conta;
 import com.sun.istack.NotNull;
-import org.joda.time.DateTimeZone;
+//import org.joda.time.DateTimeZone;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import java.util.Date;
 import java.util.UUID;
 
-public abstract class Ganho {
+abstract class Ganho {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
@@ -20,5 +21,5 @@ public abstract class Ganho {
     private Double valor;
     @OneToMany
     private Conta conta;
-    private DateTimeZone data_entrada;
+    private Date data_entrada;
 }
